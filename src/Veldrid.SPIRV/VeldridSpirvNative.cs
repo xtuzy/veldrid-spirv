420171bdd92b8b18dcd5d35bdfb7321b9cb7621e
yang zhou <xtuzhouyang@outlook.com>
2022-11-20 15:58:54 +0800

    fix LibName for mobil platform

--- src/Veldrid.SPIRV/VeldridSpirvNative.cs
+++ src/Veldrid.SPIRV/VeldridSpirvNative.cs
@@ -4,7 +4,15 @@ namespace Veldrid.SPIRV
 {
     internal static unsafe class VeldridSpirvNative
     {
+#if IOS
+        private const string LibName = "__Internal";
+#elif ANDROID
+        private const string LibName = "veldrid-spirv";
+#elif MACCATALYST
         private const string LibName = "libveldrid-spirv";
+#else
+        private const string LibName = "libveldrid-spirv";
+#endif
 
         [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
         public static extern CompilationResult* CrossCompile(CrossCompileInfo* info);
